5b3e309a7ca05f8b900ec9949b76efae43957ce9
Artemio Cabelin <artemiocabelin@gmail.com>
2017-06-28 13:24:39 -0700

    added more stuff

--- weather.js
+++ weather.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-    
+    console.log('hello');
+    $('#target').submit(function(e){
+        e.preventDefault;
+        
+        var city = $('#city').val();
+
+        $.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=c5d364a1743b6a69d0ec28484d414efa', function(res) {
+            $('#view').remove();
+            var name = res.name;
+            var temp = res.main.temp;
+            $('#main').append('<div id="view"><h1>'+name+'</h1><p>Temperature: '+ temp +'</p>');
+        });
+        return false;
+    });
+
+
 });
